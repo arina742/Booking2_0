4d84ce68b0d08ea17f4f1db5b745a66a6df8e263
arina742 <arinazav02@gmail.com>
2025-04-06 17:54:34 +0300

    Create pages: home, booking, contact, about Add templates for menu and footer

---
+++ src/main/java/com/example/booking/controller/MainController.java
@@ -0,0 +1,35 @@
+package com.example.booking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("pageTitle", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("pageTitle", "О нас");
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Model model) {
+        model.addAttribute("pageTitle", "Контакты");
+        return "contact";
+    }
+
+    @GetMapping("/booking")
+    public String booking(Model model) {
+        model.addAttribute("pageTitle", "Бронирование");
+        return "booking";
+    }
+}
---
+++ src/test/java/com/example/booking/Booking20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Booking20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
