e154cb70e4dcde5bd99d5e80c4a6b789f67eeb91
arina742 <arinazav02@gmail.com>
2025-04-06 20:27:00 +0300

    Add class User for information about booking

---
+++ src/main/java/com/example/booking/model/User.java
@@ -0,0 +1,53 @@
+package com.example.booking.model;
+
+public class User {
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private String bookingDate;
+    private int numberOfHours;
+
+    public User(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public int getNumberOfHours() {
+        return numberOfHours;
+    }
+
+    public void setNumberOfHours(int numberOfHours) {
+        this.numberOfHours = numberOfHours;
+    }
+}
