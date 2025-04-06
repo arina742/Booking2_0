6929420d805d839038bbc7065962cbfdffb20a59
arina742 <arinazav02@gmail.com>
2025-04-06 22:15:26 +0300

    Add description on main page about company

--- src/main/java/com/example/booking/controller/PageController.java
+++ src/main/java/com/example/booking/controller/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class MainController {
+public class PageController {
 
     @GetMapping("/")
     public String home(Model model) {
